450b4a658f5448c8d8bfbdd10da418441173dd05
guidev1911 <guilhermereal1911@gmail.com>
2025-08-26 01:03:58 -0300

    entidade produto e conexão com banco mysql

---
+++ src/main/java/com/guidev1911/ecommerce/model/Produto.java
@@ -0,0 +1,83 @@
+package com.guidev1911.ecommerce.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(length = 500)
+    private String descricao;
+
+    @NotNull
+    @Positive
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal preco;
+
+    @NotNull
+    @Positive
+    @Column(nullable = false)
+    private Integer estoque;
+
+    public Produto() {}
+
+    public Produto(String nome, String descricao, BigDecimal preco, Integer estoque) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Integer getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(Integer estoque) {
+        this.estoque = estoque;
+    }
+}
