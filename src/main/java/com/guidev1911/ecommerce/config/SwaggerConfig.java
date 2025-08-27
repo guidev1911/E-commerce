d988bf10294a8537c2ed09b1922ff29c761b81cb
guidev1911 <guilhermereal1911@gmail.com>
2025-08-27 17:01:32 -0300

    swagger basico

---
+++ src/main/java/com/guidev1911/ecommerce/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.guidev1911.ecommerce.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API E-commerce")
+                        .version("1.0")
+                        .description("API RESTful de gerenciamento de produtos, usando Spring Boot, DTOs e MapStruct"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
