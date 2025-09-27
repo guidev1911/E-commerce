fef1301a368a2c9dc9ab1bd33f0fa3bdfb318cf2
guidev1911 <guilhermereal1911@gmail.com>
2025-09-27 16:32:21 -0300

    flyway migrations produtos/categorias

---
+++ src/main/resources/db/migration/V1__inserir_categorias.sql
@@ -0,0 +1,6 @@
+INSERT INTO categorias (nome, descricao) VALUES
+('Hardware', 'Peças de computador, componentes internos e periféricos'),
+('Software', 'Sistemas operacionais, aplicativos e ferramentas digitais'),
+('Games', 'Jogos digitais, consoles e acessórios gamer'),
+('Redes', 'Equipamentos e soluções de conectividade, como roteadores e switches'),
+('Eletrônicos', 'Dispositivos eletrônicos de consumo, como TVs, monitores e fones de ouvido');
