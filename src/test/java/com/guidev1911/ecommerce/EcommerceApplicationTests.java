6a24e5ccb481ca3eedaba86bd14fbc78561558b0
guidev1911 <guilhermereal1911@gmail.com>
2025-09-30 02:10:50 -0300

    ignorando flyway nos testes

--- src/test/java/com/guidev1911/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/guidev1911/ecommerce/EcommerceApplicationTests.java
@@ -3,13 +3,33 @@ package com.guidev1911.ecommerce;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(properties = {
+		"spring.flyway.enabled=false"
+})
 @ActiveProfiles("test")
 class EcommerceApplicationTests {
+
+	@Container
+	static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
+			.withDatabaseName("testdb")
+			.withUsername("test")
+			.withPassword("test");
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", mysql::getJdbcUrl);
+		registry.add("spring.datasource.username", mysql::getUsername);
+		registry.add("spring.datasource.password", mysql::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
 	}
-
 }
